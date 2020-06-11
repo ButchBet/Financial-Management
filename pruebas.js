f8b548b90ae0602f179ca832aef654559bfe4667
Kevin Alejandro Salazar <53120163+ButchBet@users.noreply.github.com>
2020-06-10 21:18:40 -0500

    Added 2 new functions
    secondValidation and secondChoose, parameters of the validation function deleted.

--- pruebas.js
+++ pruebas.js
@@ -7,11 +7,11 @@ egress = Number(prompt("Which are the expenditures at this month?"))
 entryLessEgress = entry - egress;
 
 function run() {
-  validation(entryLessEgress)
+  validation()
 }
 
 //Here the customer choose what does want to do whit the money
-function validation(entryLessEgress){
+function validation(){
 
 if(entry > egress || entry > 0) {
 valid = Number(prompt("You have " + entryLessEgress + " dollars, Would you like to do something whit this money?, Y:1/N:2"))
@@ -80,5 +80,75 @@ function choose(a){
     }
     }
   }
+
+//The secondValidation works to repit a the petition of use the money with something, the firts validations did't work because
+//when the total was > to 0 it returned  entryLessEgress again instead the new total. 
+function secondValidation(total){
+   if(total > 0) {
+   valid = Number(prompt("You have " + total + " dollars, Would you like to do something whit this money?, Y:1/N:2"))
+   y = 1
+   n = 2
+
+     if(valid === y){
+     option = Number(prompt("Great, you have three options: Invest: 1, Speend: 2, Save in a bank: 3"))
+     secondChoose(total)
+
+     }
+     else if(valid === n){
+       alert("Great, see you the next time")
+     }
+     else{
+      alert("All right, see you next")
+     }
+   }
+   else if(entry === egress){
+     alert("Great, you have paid all")
+   }
+   else{
+     alert("You don't have the necessary monery to pay the egresses")
+   }
+ }
+
+//This secondChoose is necessary to return the value subtotal when the first choose is completed. 
+ function secondChoose(subtotal){
+   i = 1
+   s = 2
+   b = 3
+     if(option === i){
+       investingMoney = Number(prompt("Ok, how much money would you like investing?, actually you have " + subtotal  + " dollars"))
+       alert("Good, then we are going to invest "  + investingMoney)
+       subtotal = subtotal - investingMoney
+
+
+      if(subtotal > 0){
+        return secondValidation(subtotal)
+       }
+       else{
+         alert("Great, have a nice")
+       }
+       }
+      else if(option === s){
+       spendMoney = Number(prompt("Ok, how much would you like spending?, actually you have " + subtotal + " dollars"))
+       alert("Good, then we are gong to spend " + spendMoney)
+       subtotal = subtotal - spendMoney
+       if(subtotal > 0){
+        return secondValidation(subtotal)
+       }
+       else{
+         alert("Great, have a nice")
+       }
+     }
+      else if(option === b){
+      saveBank = Number(prompt("Great, how much Would you like saving?, actually you have " + subtotal + " dollars"))
+      alert("Good, then we are goning to save " + saveBank)
+      subtotal = subtotal - saveBank
+      if(subtotal > 0){
+        return secondValidation(subtotal)
+     }
+     else{
+       alert("Great, have a nice")
+     }
+     }
+   }
 
 run()
