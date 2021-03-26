7c3ab991d72bc121d98adebbaebee7de377640a1
Butch <butchbet01@gmail.com>
2021-03-26 18:39:01 -0500

    financial management editing finisehd

--- financial_management/js/financial_management.js
+++ financial_management/js/financial_management.js
@@ -1,154 +1,86 @@
-//I'm writing a sistem tha help you choosing what doing with your monety, It depends of your entry and egress whitin a month.
 
+//I'm writing a little program tha helps people choosing what to do with the  money, It depends of your entry and egress whitin a month.
 
-alert("Hello, I'm here to help you with your management")
-entry = Number(prompt("what is the entry this month?"))
-egress = Number(prompt("Which are the expenditures at this month?"))
+var entry=0, egress=0, entryLessEgress=0, valid=0, option=0;
+var subtotal=0, investMoney=0, spendMoney=0, saveBank=0;
+
+alert("Hello, I'm here to help you with your management");
+entry = Number(prompt("Enter this  month's entry"));
+egress = Number(prompt("Enter the egress"));
 entryLessEgress = entry - egress;
 
-function run() {
-  validation()
+function run(){
+    validation(entryLessEgress);
 }
 
-//Here the customer choose what does want to do whit the money
-function validation(){
-
-if(entry > egress || entry > 0) {
-valid = Number(prompt("You have " + entryLessEgress + " dollars, Would you like to do something whit this money?, Y:1/N:2"))
-y = 1
-n = 2
-
-  if(valid === y){
-  option = Number(prompt("Great, you have three options: Invest: 1, Speend: 2, Save in a bank: 3"))
-  choose()
-
-  }
-  else if(valid === n){
-    alert("Great, see you the next time")
-  }
-  else{
-   alert("All right, see you next")
-  }
-}
-else if(entry === egress){
-  alert("Great, you have paid all")
-}
-else{
-  alert("You don't have the necessary monery to pay the egresses")
-}
+//Here the customer choose what does He want to do whit the money
+function validation(ele){
+    if(ele > 0){	
+        valid = Number(prompt("You have " + ele + " free dollars, would you like to do something with this money? \n1. Yes \n2. No"));
+	switch(valid){
+	case 1:
+	    option = Number(prompt("Great, you have three options \n1. Invest \n2. Speend\n3. Save in a bank"));
+            choose(option, ele);
+	    break;
+	case 2:
+	    alert("Great, see you the next time.");
+	    break;
+	default:
+	    alert("Incorrect option.");
+	    validation(ele);
+	    break;
+    }
+    }
+    else if(ele == 0){
+        alert("Great, you have paid all, see you next time.");
+    }
+    else{
+	alert("You don't have the necessary monery to pay the egresses.");
+	// Here we can expand the code to add a option to get a credit
+    }
 }
 
 
 //choosing function, the coustomer have 3 options, investing, spend or saving.
-function choose(a){
-  i = 1
-  s = 2
-  b = 3
-    if(option === i){
-      investingMoney = Number(prompt("Ok, how much money would you like investing?, actually you have " + entryLessEgress  + " dollars"))
-      alert("Good, then we are going to invest "  + investingMoney)
-      subtotal = entryLessEgress - investingMoney
+function choose(op, ele){
+    switch(op){
+    case 1:
+	investMoney = Number(prompt("Ok, how much money would you like investing?, actually you have " + ele  + " dollars."));
+        alert("Good, then we are going to invest "  + investMoney + " dollars.");
+        subtotal = ele - investMoney;
 
+	analysis(subtotal);
+	
+	break;
+    case 2:
+	spendMoney = Number(prompt("Ok, how much would you like spending?, actually you have " + ele + " dollars."));
+        alert("Good, then we are going to spend " + spendMoney + " dollars.");
+        subtotal = ele - spendMoney
 
-      if(subtotal > 0){
-        secondValidation(subtotal)
-      }
-      else{
-        alert("Great, have a nice")
-      }
-      }
-     else if(option === s){
-      spendMoney = Number(prompt("Ok, how much would you like spending?, actually you have " + entryLessEgress + " dollars"))
-      alert("Good, then we are going to spend " + spendMoney)
-      subtotal = entryLessEgress - spendMoney
-      if(subtotal > 0){
-        secondValidation(subtotal)
-      }
-      else{
-        alert("Great, have a nice")
-      }
+	analysis(subtotal);
+	
+	break;
+    case 3:
+	saveBank = Number(prompt("Great, how much Would you like saving?, actually you have " + ele + " dollars."));
+        alert("Good, then we are going to save " + saveBank + " dollars.");
+        subtotal = ele - saveBank;
+	
+	analysis(subtotal);
+	
+	break;
+    default:
+	break;
     }
-     else if(option === b){
-     saveBank = Number(prompt("Great, how much Would you like saving?, actually you have " + entryLessEgress + " dollars"))
-     alert("Good, then we are going to save " + saveBank)
-     subtotal = entryLessEgress - saveBank
-     if(subtotal > 0){
-      secondValidation(subtotal)
+}
+
+
+function analysis(sub){
+    if(sub > 0){
+	validation(subtotal);
     }
     else{
-      alert("Great, have a nice")
+	alert("Great, have a nice.");
     }
-    }
-  }
-
-//The secondValidation works to repit a the petition of use the money with something, the firts validations did't work because
-//when the total was > to 0 it returned  entryLessEgress again instead the new total. 
-function secondValidation(total){
-   if(total > 0) {
-   valid = Number(prompt("You have " + total + " dollars, Would you like to do something whit this money?, Y:1/N:2"))
-   y = 1
-   n = 2
-
-     if(valid === y){
-     option = Number(prompt("Great, you have three options: Invest: 1, Speend: 2, Save in a bank: 3"))
-     secondChoose(total)
-
-     }
-     else if(valid === n){
-       alert("Great, see you the next time")
-     }
-     else{
-      alert("All right, see you next")
-     }
-   }
-   else if(entry === egress){
-     alert("Great, you have paid all")
-   }
-   else{
-     alert("You don't have the necessary monery to pay the egresses")
-   }
- }
-
-//This secondChoose is necessary to return the value subtotal when the first choose is completed. 
- function secondChoose(subtotal){
-   i = 1
-   s = 2
-   b = 3
-     if(option === i){
-       investingMoney = Number(prompt("Ok, how much money would you like investing?, actually you have " + subtotal  + " dollars"))
-       alert("Good, then we are going to invest "  + investingMoney)
-       subtotal = subtotal - investingMoney
-
-
-      if(subtotal > 0){
-        return secondValidation(subtotal)
-       }
-       else{
-         alert("Great, have a nice")
-       }
-       }
-      else if(option === s){
-       spendMoney = Number(prompt("Ok, how much would you like spending?, actually you have " + subtotal + " dollars"))
-       alert("Good, then we are gong to spend " + spendMoney)
-       subtotal = subtotal - spendMoney
-       if(subtotal > 0){
-        return secondValidation(subtotal)
-       }
-       else{
-         alert("Great, have a nice")
-       }
-     }
-      else if(option === b){
-      saveBank = Number(prompt("Great, how much Would you like saving?, actually you have " + subtotal + " dollars"))
-      alert("Good, then we are goning to save " + saveBank)
-      subtotal = subtotal - saveBank
-      if(subtotal > 0){
-        return secondValidation(subtotal)
-     }
-     else{
-       alert("Great, have a nice")
-     }
-     }
-   }
+}
 
 run()
